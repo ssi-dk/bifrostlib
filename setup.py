f8c70dff331952e9e04886579df2c0d01c5a8346
Kim Ng <kimleeng@gmail.com>
2020-12-02 10:07:39 +0100

    Bump version: 2.1.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrostlib',
-    version='2.1.0',
+    version='version='2.1.0'',
     description='Datahandling functions for bifrost (later to be API interface)',
     url='https://github.com/ssi-dk/bifrost/tree/master/lib/bifrostlib',
     author="Kim Ng, Martin Basterrechea",
